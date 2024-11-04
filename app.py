d4c6b5b1bbca04ed0a837a36f9285f316a39c4b1
Jeen Namkung <nkjeen3876@gmail.com>
2024-11-03 18:56:30 -0800

    delete comment

--- app.py
+++ app.py
@@ -1,26 +1,3 @@
-# from flask import Flask, request, jsonify
-# from transformers import AutoModelForCausalLM, AutoTokenizer
-# import torch
-
-# app = Flask(__name__)
-
-# model_name = "facebook/bart-large-cnn"
-# tokenizer = AutoTokenizer.from_pretrained(model_name)
-# model = AutoModelForCausalLM.from_pretrained(model_name)
-
-# @app.route("/", methods=["POST"])
-# def generate_text():
-#     data = request.json
-#     prompt = data.get("prompt", "")
-#     inputs = tokenizer(prompt, return_tensors="pt")
-#     outputs = model.generate(**inputs, max_length=50)
-#     generated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
-#     return jsonify({"response": generated_text})
-
-# if __name__ == "__main__":
-#     app.run(port=5000)
-
-
 from flask import Flask, request, jsonify
 from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
